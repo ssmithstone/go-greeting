b86fe4a9fbe092350666ebac11fd65d511363fb3
stephen smithstone <ssmithstone@me.com>
2024-04-10 05:38:22 +1000

    Add time information to greeting
    The greeting program has been extended to include the current time. The time is displayed in Unix timestamp format, a human-readable format, and broken down into hours, minutes, and seconds for convenience. The date is also shown, broken down into year, month, and day.

--- greeting.go
+++ greeting.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	name := "Joe"
-	fmt.Printf("Hello, %s!", name)
+	fmt.Printf("Hello, %s!\n", name)
+	printCurrentTime()
+}
+
+func printCurrentTime() {
+	t := time.Now()
+	fmt.Printf("Current time: %d\n", t.Unix()) // current time in seconds since 1970
+	fmt.Printf("Current Formatted time: %s\n", t.Format(time.RFC822))
+	hour, m, sec := t.Clock() // localtime clock
+	fmt.Printf("Hour: %d\nMinute: %d\nSecond: %d\n", hour, m, sec)
+	year, month, day := t.Date() // localtime date
+	fmt.Printf("Year: %d\nMonth: %d\nDay: %d\n", year, month, day)
 }
